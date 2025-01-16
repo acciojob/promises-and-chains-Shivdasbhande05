11014dea62c171ef4db20955af2fc033ec2182ba
Shivdasbhande05 <shivdasbhande@gmail.com>
2025-01-16 14:12:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+const age = document.getElementById("age");
+const name = document.getElementById("name");
+const btn = document.getElementById("btn");
+
+function promise (age) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() =>{
+            if(age < 18){
+                reject(`oh sorry ${name.value}. You aren't old enough`);
+             } else {
+                resolve(`Welcome, ${name.value}. You can vote`);
+             }
+        },4000);
+    });
+}
+
+btn.addEventListener("click", () => {
+    promise(age.value).then((resolve) =>{
+        alert(resolve)
+    }).catch((reject) => {
+        alert(reject);
+    });
+});
